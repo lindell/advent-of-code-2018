5c7173a7db5a222ab1b2e04a74b4cef7b5cd3e2e
Johan Lindell <johan@lindell.me>
2018-12-02 09:56:59 +0100

    2 December

---
+++ days/2/1.js
@@ -0,0 +1,26 @@
+const aocLoader = require('aoc-loader');
+
+function solver(data) {
+	const lines = data.split('\n').map(l => l.split(''));
+	let twice = 0;
+	let thrice = 0;
+	for (const l of lines) {
+		const numValues = {};
+		for (const v of l) {
+			numValues[v] = (numValues[v] || 0) + 1;
+		}
+
+		if (Object.values(numValues).includes(2)) {
+			twice++;
+		}
+		if (Object.values(numValues).includes(3)) {
+			thrice++;
+		}
+	}
+
+	return twice * thrice;
+}
+
+aocLoader(2018, 2).then(solver)
+	.then((ans) => console.log(`Answer is: ${ans}`))
+	.catch(console.log);
---
+++ days/2/2.js
@@ -0,0 +1,37 @@
+const aocLoader = require('aoc-loader');
+
+function solver(data) {
+	const lines = data.split('\n').map(l => l.split(''));
+	
+	for (let i = 0; i < lines.length; i++) {
+		for (let j = i + 1; j < lines.length; j++) {
+			if (diffLines(lines[i], lines[j]) === 1) {
+				return shared(lines[i], lines[j]).join('');
+			}
+		}
+	}
+}
+
+function shared(l1, l2) {
+	const res = [];
+	for (let i = 0; i < l1.length; i++) {
+		if (l1[i] === l2[i]) {
+			res.push(l1[i]);
+		}
+	}
+	return res;
+}
+
+function diffLines(l1, l2) {
+	let diff = 0;
+	for (let i = 0; i < l1.length; i++) {
+		if (l1[i] !== l2[i]) {
+			diff++;
+		}
+	}
+	return diff;
+}
+
+aocLoader(2018, 2).then(solver)
+	.then((ans) => console.log(`Answer is: ${ans}`))
+	.catch(console.log);
